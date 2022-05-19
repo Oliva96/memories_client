6282b3c62fe05ac1d38fff2adcc5436d5e2b8d0b
Oliva96 <oliva961206@gmail.com>
2022-05-19 19:08:14 -0400

    deploy

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,151 @@
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import { GoogleLogin } from "@react-oauth/google";
+
+import LockOutlineIcon from "@material-ui/icons/LockOutlined";
+import jwt_decode from "jwt-decode";
+
+import useStyles from "./styles";
+import Input from "./Input";
+import { useDispatch } from "react-redux";
+import { AUTH } from "../../constants/actionTypes";
+import { useHistory } from "react-router-dom";
+import { signIn, signUp } from "../../actions/auth";
+
+const Auth = () => {
+  const classes = useStyles();
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isSignUp) {
+      dispatch(signUp(formData, history));
+    } else {
+      dispatch(signIn(formData, history));
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const switchMode = () => {
+    setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+  };
+
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+
+  const googleSuccess = async (res) => {
+    const token = res.credential;
+    const result = jwt_decode(token);
+    try {
+      dispatch({ type: AUTH, data: { token, result } });
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlineIcon />
+        </Avatar>
+        <Typography cariant="h5">{isSignUp ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignUp && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignUp && (
+              <Input
+                name="confirmPassword"
+                label="Reapeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignUp ? "Sign Up" : "Sign In"}
+          </Button>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <GoogleLogin
+              size="large"
+              width="250px"
+              onSuccess={googleSuccess}
+              onError={(error) => {
+                console.log(error);
+              }}
+            />
+          </div>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignUp
+                  ? "Already have an account? Sign In"
+                  : "Don't have an account Sign Up"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Auth;
